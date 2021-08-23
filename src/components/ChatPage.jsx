54e7a6c385ddb8a5cce9b203a6a5db01f0d1a1bf
vvvhatislove <vladenisov1234@yandex.ru>
2021-08-23 17:09:21 +0300

    data fetching added

--- src/components/ChatPage.jsx
+++ src/components/ChatPage.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import routes from '../routes.js';
 
-export default () => (
-  <h4>
-    This is chat page!
-  </h4>
-);
+const getAuthHeader = () => {
+  const userId = JSON.parse(localStorage.getItem('userId'));
+
+  if (userId && userId.token) {
+    return { Authorization: `Bearer ${userId.token}` };
+  }
+
+  return {};
+};
+
+export default () => {
+  const [content, setContent] = useState('');
+  useEffect(() => {
+    const fetchContent = async () => {
+      const { data } = await axios.get(routes.dataPath(), { headers: getAuthHeader() });
+      setContent(data);
+    };
+
+    fetchContent();
+  }, []);
+
+  console.log(content);
+
+  return content && <p>Look for content in console</p>;
+};
