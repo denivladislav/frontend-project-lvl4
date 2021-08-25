5e31f7fc393fd971519d85ba3f51c76f07020e09
vvvhatislove <vladenisov1234@yandex.ru>
2021-08-25 17:20:00 +0300

    socket provider added

--- src/init.jsx
+++ src/init.jsx
@@ -1,14 +1,21 @@
 import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
 import React from 'react';
+import io from 'socket.io-client';
 import { Provider } from 'react-redux';
 import App from './components/App.jsx';
 import store from './store.js';
+import socketContext from './contexts/socketContext.jsx';
 
 import '../assets/application.scss';
 
-export default () => (
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+export default () => {
+  const socket = new io();
+  return (
+    <socketContext.Provider value={socket}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </socketContext.Provider>
+  );
+};
