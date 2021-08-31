2357b41edae51b6467ba8658f73669ed5606449f
vvvhatislove <vladenisov1234@yandex.ru>
2021-08-31 10:38:45 +0300

    ChatPage renamed to Hexlet Chat

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -80,7 +80,7 @@ export default () => (
       <Col className="d-flex flex-column h-100">
         <Navbar bg="light" expand="lg">
           <Container>
-            <Navbar.Brand as={Link} to="/">ChatPage</Navbar.Brand>
+            <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
             <Nav className="me-auto">
               <Nav.Link as={Link} to="/login">LoginPage</Nav.Link>
               <Nav.Link as={Link} to="/signup">SignUpPage</Nav.Link>
