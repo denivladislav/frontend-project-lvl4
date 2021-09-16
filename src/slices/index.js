0cacbb8df5610e774956a8d3029b8933c791d8ea
vvvhatislove <vladenisov1234@yandex.ru>
2021-09-16 18:06:23 +0300

    store moved to slices

--- src/slices/index.js
+++ src/slices/index.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import channelsReducer from './slices/channelsSlice.js';
-import messagesReducer from './slices/messagesSlice.js';
-import modalReducer from './slices/modalSlice.js';
+import channelsReducer from './channelsSlice.js';
+import messagesReducer from './messagesSlice.js';
+import modalReducer from './modalSlice.js';
 
 export default configureStore({
   reducer: {
